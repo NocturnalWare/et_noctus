c7921b170b9d5bf4eca52e40ce50f77afe64cbf5
Jota Vares <joe@eternallynocturnal.com.com>
2016-06-19 15:07:59 -0400

    moves session cart setting to new middleware

--- app/Http/Middleware/SetCart.php
+++ app/Http/Middleware/SetCart.php
@@ -17,12 +17,16 @@ class SetCart
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if(empty(Session::get('cart_id'))){
+            Session::set('cart_id', str_random(32));
+        }
+
         view()->share('cart_quantity', \App\Carts\Cart::where('cart_id', \Session::get('cart_id'))->sum('quantity'));
         
         \JavaScript::put([
             'cart_quantity' => \App\Carts\Cart::where('cart_id', \Session::get('cart_id'))->sum('quantity'),
         ]);
-        
+
         return $next($request);
     }
 }
--- resources/views/layouts/set-cart-id.blade.php
+++
@@ -1,3 +0,0 @@
-@if(empty(Session::get('cart_id')))
-  {!! Session::set('cart_id', str_random(32)) !!}
-@endif
