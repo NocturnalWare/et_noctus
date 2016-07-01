b9a8f341235f49d8ad409d6f5bbd2003f5202782
Jota Vares <joe@eternallynocturnal.com.com>
2016-07-01 19:28:52 -0400

    lol missed e

--- resources/views/auth/instagram.blade.php
+++ resources/views/auth/instagram.blade.php
@@ -5,7 +5,7 @@
 
 IGTEST
 
-@forach($ig->data as $image)
+@foreach($ig->data as $image)
 <div class="col-md-4 col-xs-12">
 	<img class="img-responsive" style="height:300px" src="{{$image->link}}">
 </div>
