9a8dba4e26eb44c2b052511f9bbfd4fdeafc3807
Jota Vares <joe@eternallynocturnal.com.com>
2016-07-01 18:54:12 -0400

    backpedal

--- app/Http/Controllers/Auth/IGController.php
+++ app/Http/Controllers/Auth/IGController.php
@@ -10,28 +10,27 @@ use App\Http\Controllers\Controller;
 class IGController extends Controller
 {
     public function authenticate(Request $request){
-    	return Socialite::with('instagram')->redirect()->route('welcome');
-  //   	if(empty(\Session::get('ig_auth'))){
-	 //    	$curl = curl_init(); 
-		// 	curl_setopt($curl, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
-		// 	curl_setopt($curl, CURLOPT_POST, true);
-		// 	curl_setopt($curl, CURLOPT_POSTFIELDS, [
-		// 		'client_id' => ENV('INSTAGRAM_ID'),
-		// 		'client_secret' => ENV('INSTAGRAM_KEY'),
-		// 		'grant_type' => 'authorization_code',
-		// 		'redirect_uri' => 'https://staging11.eternallynocturnal.com/ig/auth',
-		// 		'code' => $request->get('code'),
-		// 	]);
+    	if(empty(\Session::get('ig_auth'))){
+	    	$curl = curl_init(); 
+			curl_setopt($curl, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, [
+				'client_id' => ENV('INSTAGRAM_ID'),
+				'client_secret' => ENV('INSTAGRAM_KEY'),
+				'grant_type' => 'authorization_code',
+				'redirect_uri' => 'https://staging11.eternallynocturnal.com/ig/auth',
+				'code' => $request->get('code'),
+			]);
 
-		// 	$response = curl_exec($curl);
+			$response = curl_exec($curl);
 
-		// 	$json = json_decode($response, true);
-		// 	// dd($json);
-		// 	\Session::put('ig_auth', 'access_granted');
-		// 	// \Session::put('ig_auth', $response['access_token']);
-  //   	}
+			$json = json_decode($response->get(), true);
+			// dd($json);
+			// \Session::put('ig_auth', $json['access_token']);
+			\Session::put('ig_auth', $json['access_token']);
+    	}
 
-		// return redirect()->route('welcome');
+		return redirect()->route('welcome');
   
 
 		// $curl_setopt("client_id=ENV('INSTAGRAM_KEY')");
