a68fab7ac3a9ec324470bcce388eb5e0b3ad2001
Jota Vares <joe@eternallynocturnal.com.com>
2016-07-01 18:13:06 -0400

    lets see what happens!

--- app/Http/Controllers/Auth/IGController.php
+++ app/Http/Controllers/Auth/IGController.php
@@ -10,6 +10,24 @@ use App\Http\Controllers\Controller;
 class IGController extends Controller
 {
     public function authenticate(Request $request){
-    	dd($request->get('code'));
+    	$curl = curl_init(); 
+		curl_setopt($curl, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, [
+			'client_id' => ENV('INSTAGRAM_ID'),
+			'client_secret' => ENV('INSTAGRAM_KEY'),
+			'grant_type' => 'authorization_code',
+			'redirect_uri' => 'https://staging11.eternallynocturnal.com/ig/auth',
+			'code' => $request->get('code'),
+		]);
+
+		dd($curl->exec());
+
+		// $curl_setopt("client_id=ENV('INSTAGRAM_KEY')");
+	 //    -F 'client_secret=CLIENT_SECRET' \
+	 //    -F 'grant_type=authorization_code' \
+	 //    -F 'redirect_uri=AUTHORIZATION_REDIRECT_URI' \
+	 //    -F 'code=CODE' \
+  //   	dd($request->get('code'));
     }
 }
