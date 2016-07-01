8f27b366c27e90226ea6f8a2c7535eff0d00e913
Jota Vares <joe@eternallynocturnal.com.com>
2016-07-01 18:48:12 -0400

    socialite??

--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Vinkla\Instagram\InstagramServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
         'Laracasts\Utilities\JavaScript\JavaScriptServiceProvider',
         'Maknz\Slack\SlackServiceProvider',
     ],
@@ -205,6 +206,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Slack'     => 'Maknz\Slack\Facades\Slack',
         'Instagram' => Vinkla\Instagram\Facades\Instagram::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,7 +24,7 @@
   @yield('javascript')
   TEST IG STUFF
   @if(!empty(Session::get('ig_auth')))
-    {{Instagram::tags('stayawaketocreate')->get(21)}}
+    <iframe src="https://api.instagram.com/v1/tags/stayawaketocreate?access_token={{\Laravel\Socialite\User->token}}"></iframe>
   @else
     <a href="https://api.instagram.com/oauth/authorize/?client_id={{ENV('INSTAGRAM_ID')}}&redirect_uri=https://staging11.eternallynocturnal.com/ig/auth&response_type=code">iglogin??</a>
   @endif
