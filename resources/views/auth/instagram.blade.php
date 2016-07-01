13f62ae76c24224be43568cf55d45f468b50bb96
Jota Vares <joe@eternallynocturnal.com.com>
2016-07-01 19:31:04 -0400

    no data property

--- resources/views/auth/instagram.blade.php
+++ resources/views/auth/instagram.blade.php
@@ -5,7 +5,7 @@
 
 IGTEST
 
-@foreach($ig->data as $image)
+@foreach($ig as $image)
 <div class="col-md-4 col-xs-12">
 	<img class="img-responsive" style="height:300px" src="{{$image->link}}">
 </div>
