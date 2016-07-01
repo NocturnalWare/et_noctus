5ef066bfc34f91f52a7270f19686a2ceb66a49d9
Jota Vares <joe@eternallynocturnal.com.com>
2016-07-01 18:19:07 -0400

    access granted. test facade

--- app/Http/Controllers/Auth/IGController.php
+++ app/Http/Controllers/Auth/IGController.php
@@ -10,18 +10,24 @@ use App\Http\Controllers\Controller;
 class IGController extends Controller
 {
     public function authenticate(Request $request){
-    	$curl = curl_init(); 
-		curl_setopt($curl, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, [
-			'client_id' => ENV('INSTAGRAM_ID'),
-			'client_secret' => ENV('INSTAGRAM_KEY'),
-			'grant_type' => 'authorization_code',
-			'redirect_uri' => 'https://staging11.eternallynocturnal.com/ig/auth',
-			'code' => $request->get('code'),
-		]);
+    	if(empty(\Session::get('ig_auth'))){
+	    	$curl = curl_init(); 
+			curl_setopt($curl, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, [
+				'client_id' => ENV('INSTAGRAM_ID'),
+				'client_secret' => ENV('INSTAGRAM_KEY'),
+				'grant_type' => 'authorization_code',
+				'redirect_uri' => 'https://staging11.eternallynocturnal.com/ig/auth',
+				'code' => $request->get('code'),
+			]);
 
-		dd(curl_exec($curl));
+			$response = curl_exec($curl);
+
+			\Session::put('ig_auth', $response->access_token);
+    	}
+
+		return redirect()->route('welcome');
 
 		// $curl_setopt("client_id=ENV('INSTAGRAM_KEY')");
 	 //    -F 'client_secret=CLIENT_SECRET' \
