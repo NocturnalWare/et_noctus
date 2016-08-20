3d6e0b2533a9a2e4604c328fcd601a7cd73ab342
Jota Vares <joe@eternallynocturnal.com.com>
2016-08-20 13:05:32 -0400

    weird.

--- resources/views/auth/instagram.blade.php
+++ resources/views/auth/instagram.blade.php
@@ -10,8 +10,8 @@
 
 	    <div class="item active">
 		    @foreach($instagram->resp1 as $igram)
-				<div class="col-md-3 col-xs-12" style="padding-bottom:15px;color:#000;height:585px">
-		      		<div class="well">
+				<div class="col-md-3 col-xs-12" style="padding-bottom:15px;color:#000;">
+		      		<div class="well" style="height:585px">
 						<img class="img-responsive" src="{{$igram['images']['standard_resolution']['url']}}">
 						<p>{{$igram['caption']['text']}}</p>
 					</div>
@@ -22,8 +22,8 @@
 		@foreach($instagram->images as $image)
 		    <div class="item">
 		    	@foreach($image as $igram)
-				<div class="col-md-3 col-xs-12" style="padding-bottom:15px;color:#000;height:585px">
-					<div class="well">
+				<div class="col-md-3 col-xs-12" style="padding-bottom:15px;color:#000;">
+					<div class="well" style="height:585px">
 						<img class="img-responsive" src="{{$igram['images']['standard_resolution']['url']}}">
 						<p>{{$igram['caption']['text']}}</p>
 					</div>
