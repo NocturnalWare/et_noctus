aef4e032e93d01613100450e29473938daac3384
Jota Vares <joe@eternallynocturnal.com.com>
2016-12-23 00:41:59 -0500

    image size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
                         </span>
                     </div>
                     <div class="col-xs-4">
-                        <img class="img-responsive" style="border-radius:12px;max-height: 250px;" src="https://scontent-ort2-1.xx.fbcdn.net/v/t31.0-8/15540926_624810791040104_1557989138155771777_o.jpg?oh=ae9481f68b5176e3f83c0fa5a2b968ea&oe=58E6ED8E">
+                        <img class="img-responsive" style="border-radius:12px;max-height: 320px;" src="https://scontent-ort2-1.xx.fbcdn.net/v/t31.0-8/15540926_624810791040104_1557989138155771777_o.jpg?oh=ae9481f68b5176e3f83c0fa5a2b968ea&oe=58E6ED8E">
                     </div>
                 </div>
             </div>
@@ -28,7 +28,7 @@
                         </span>
                     </div>
                     <div class="col-xs-4">
-                        <img class="img-responsive" style="border-radius:12px;max-height: 250px;" src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/12195932_459269320927586_4558979120605616634_n.jpg?oh=fd096bcd04e124976c28370a2fb0fc3a&oe=58F175DA">
+                        <img class="img-responsive" style="border-radius:12px;max-height: 320px;" src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/12195932_459269320927586_4558979120605616634_n.jpg?oh=fd096bcd04e124976c28370a2fb0fc3a&oe=58F175DA">
                     </div>
                 </div>
             </div>
